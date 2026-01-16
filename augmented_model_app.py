90067fd6059ef7738e3dfc4f18c1bba915f1f94d
Sherzadoo <sherzadzabihullahg@gmail.com>
2026-01-16 16:35:29 +0100

    augmented_model_app

--- augmented_model_app.py
+++ augmented_model_app.py
@@ -56,8 +56,8 @@ div[data-testid="stFileUploader"] button::after {
 # Hidden paths (NO sidebar settings)
 # -----------------------------
 BASE_DIR = Path(__file__).resolve().parent
-MODEL_PATH = (BASE_DIR / "models" / "image_classification_model_linux.keras").resolve()
-CLASSES_PATH = (BASE_DIR / "class_names.json").resolve()
+MODEL_PATH = (BASE_DIR / "models" / "augmented_model_linux.keras").resolve()
+CLASSES_PATH = (BASE_DIR / "classes.json").resolve()
 
 # -----------------------------
 # Rules / thresholds
